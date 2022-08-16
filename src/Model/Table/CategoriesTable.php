902df5bc554a56f95d92c86c822a5497f9c65d24
Alan Santos <santos.alansousa@gmail.com>
2022-08-15 21:10:42 -0300

    Add soft delete a Categorias, removendo arquivos não utilizados

--- src/Model/Table/CategoriesTable.php
+++ src/Model/Table/CategoriesTable.php
@@ -7,6 +7,7 @@ use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use SoftDelete\Model\Table\SoftDeleteTrait;
 
 /**
  * Categories Model
@@ -31,6 +32,7 @@ use Cake\Validation\Validator;
  */
 class CategoriesTable extends Table
 {
+    use SoftDeleteTrait;
     /**
      * Initialize method
      *
